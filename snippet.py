ca3f1660a250dd170b81759b79f4348d1e824f56
Julka <julkastawiarz@MacBook-Pro-Julka.local>
2020-02-09 21:23:14 +0100

    add janitor feat

--- snippet.py
+++ snippet.py
@@ -1,7 +1,23 @@
-for i in range(10):
-        print("*", end="")
-
-print()
-for i in range(10):
-        print("*", end="")
-    
+def main():
+ password=input("Please enter your password::")
+ if password=="student":
+ student()
+ elif password=="teacher":
+ teacher()
+ elif password=="headmaster":
+Page 6 of 7
+ headmaster()
+ elif password=="janitor":
+ janitor()
+ else:
+ print("incorrect password. We cannot take you any further,
+goodbye!")
+def student():
+ print("****WELCOME STUDENT****")
+def teacher():
+ print("****WELCOME TEACHER****")
+def headmaster():
+ print("****WELCOME HEADMASTER****")
+def janitor():
+ print("****WELCOME JANITOR****")
+main()
